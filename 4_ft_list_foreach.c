1001aeaf96371899e7f41cd86ee15e1128db2ee2
cicoree <joanna.nurmis@gmail.com>
2019-06-11 10:35:18 -0700

    Create 4_ft_list_foreach.c

---
+++ 4_ft_list_foreach.c
@@ -0,0 +1,13 @@
+#include "ft_list.h" //make sure to include this prototype along with the typedef and the ifndef / define / endif in your .h
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list		*list_ptr;
+
+	list_ptr = begin_list;
+	while(list_ptr)
+	{
+		(*f)(list_ptr->data);
+		list_ptr = list_ptr->next;
+	}
+}
