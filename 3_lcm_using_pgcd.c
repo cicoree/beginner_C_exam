f1130a622eaa4581d82ca4f81ef594e1b33538aa
cicoree <joanna.nurmis@gmail.com>
2019-07-09 22:13:16 -0700

    Approved by Moulinette

--- 3_lcm_using_pgcd.c
+++ 3_lcm_using_pgcd.c
@@ -15,10 +15,10 @@ unsigned int	lcm(unsigned int a, unsigned int b)
 	unsigned int	a_cpy;
 	unsigned int	b_cpy;
 
-	a_cpy = a;
-	b_cpy = b;
+	a_cpy = a; // storing the initial value of a for later use
+	b_cpy = b; // storing the initial value of b for later use
 
-	if (a > 0 && b > 0)
+	if (a > 0 && b > 0) //this if statement block calculates the PGCD (Highest Common Factor) of a and b
 	{	
 		while (a != b)
 		{
@@ -27,7 +27,7 @@ unsigned int	lcm(unsigned int a, unsigned int b)
 			else
 				b = b - a;
 		}
-		return (a_cpy * b_cpy / a);
+		return (a_cpy * b_cpy / a); // a is PGCD. The formula for LCM is LCM(a,b) = (a*b)/PGCD(a,b)
 	}
 	else
 		return (0);
