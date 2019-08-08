768b993ccc3eccd00de238d858ba464d2ab76e51
Joanna Nurmis <joanna.nurmis@gmail.com>
2019-08-08 15:11:36 -0700

    Not verified by Moulinette

---
+++ 4-rostring.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+# define ISBLANK(c) (c == ' ' || c == '\t' || c == '\n')
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	rostring(char *str)
+{
+	int i;
+	int start;
+	int end;
+
+	i = 0;
+	while (str[i] && ISBLANK(str[i]))
+		i++; //ignore empty spaces in the front
+	start = i; // start = first char of the first word
+	while (str[i] && !ISBLANK(str[i]))
+		i++;
+	end = i; // end = last char of the first word
+	while (str[i])
+	{
+		while (str[i] && ISBLANK(str[i]))
+			i++; // moving to the next word
+		while (str[i] && !ISBLANK(str[i])) // as long as we are in a word, write that word
+		{
+			ft_putchar(str[i]); 
+			i++; 
+		}
+		ft_putchar(' '); // follow it with a single space
+	}
+	while (start < end) // now finish by writing the first word at the end
+	{
+		ft_putchar(str[start]); // location of first word being is between start and end
+		start++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		rostring(argv[1]);
+		ft_putchar('\n');
+	}
+	else
+		ft_putchar('\n');
+	return (0);
+}
